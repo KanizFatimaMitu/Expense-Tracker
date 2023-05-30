ffccfdc900c9cc71f8bddde3bcd518a1ac3647cd
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-30 22:04:26 +0600

    mapping

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,13 +1,31 @@
+import { useSelector } from "react-redux";
 import Transaction from "./Transaction";
 
 export default function Transactions() {
+    const { transactions, isLoading, isError } = useSelector((state) => state.Transaction)
+
+    let content = null
+    if (isLoading) {
+        content = <p>Loading...</p>
+    }
+    if (!isLoading && isError) {
+        content = <p className="error">There was an error occured</p>
+    }
+    if (!isLoading && !isError && transactions?.length > 0) {
+        content = transactions.map(transaction =>
+            <Transaction
+                key={transaction.id}
+                transaction={transaction}
+            />)
+    }
+    if (!isLoading && !isError && transactions.length === 0)
     return (
         <>
             <p className="second_heading">Your Transactions:</p>
 
             <div className="conatiner_of_list_of_transactions">
                 <ul>
-                    <Transaction />
+                    {content}
                 </ul>
             </div>
         </>
