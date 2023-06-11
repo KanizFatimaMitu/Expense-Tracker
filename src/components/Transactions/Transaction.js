6df73b5821a9fd0632eecc31bde0bdfee16a5588
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-12 00:56:47 +0600

    delete functionality added

--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -1,25 +1,29 @@
 import { useDispatch } from "react-redux";
 import deleteImage from "../../assets/images/delete.svg";
 import editImage from "../../assets/images/edit.svg";
-import { editActive } from "../../features/transaction/transactionSlice";
+import { editActive, removeTransaction } from "../../features/transaction/transactionSlice";
 
 export default function Transaction({ transaction }) {
-    const { name, amount, type } = transaction || {};
+    const {id, name, amount, type } = transaction || {};
     const dispatch = useDispatch();
 
     const handleEdit = () => {
         dispatch(editActive(transaction))
+    }
+
+    const handleDelete = () => {
+        dispatch(removeTransaction(id))
     }
     return (
         <li className={`transaction ${type}`}>
             <p>{name}</p>
             <div className="right">
                 <p>৳ {amount}</p>
-                <button className="link"
-                 onClick={handleEdit}>
+                <button className="link" onClick={handleEdit}>
                     <img alt="Edit" className="icon" src={editImage} />
                 </button>
-                <button className="link">
+
+                <button className="link" onClick={handleDelete}>
                     <img alt="Delete" className="icon" src={deleteImage} />
                 </button>
             </div>
